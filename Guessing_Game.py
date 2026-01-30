5f950a63e35a001285f49bfdc7365c4504fa86ba
dejicodes <adedejia329@gmail.com>
2026-01-30 14:38:30 +0100

    Fix guess number logic and input validation

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -7,26 +7,27 @@ print("🎯 Welcome to the Guessing Game!")
 print("You have 5 tries to guess the number between 1 and 100.\n")
 
 while tries > 0:
+    try:
         user_guess = int(input("Enter your guess: "))
 
         if user_guess < 1 or user_guess > 100:
             print("Please guess a number between 1 and 100.\n")
+
         elif user_guess > guess_number:
             tries -= 1
             print(f"Too high! You have {tries} trie(s) left.\n")
+
         elif user_guess < guess_number:
             tries -= 1
             print(f"Too low! You have {tries} trie(s) left.\n")
-        elif user_guess == guess_number:
-            print("Congratulations! You guessed the correct number!")
+
         else:
-            print("Invalid input. Please enter a number.\n")
+            print("Congratulations! You guessed the correct number!")
             break
+
+    except ValueError:
+        print("Invalid input! Please enter numbers only.\n")
+
 if tries == 0:
     print("💀 Game Over! You've used all your tries.")
     print("✅ The correct number was:", guess_number)
-
-
-
-
-
